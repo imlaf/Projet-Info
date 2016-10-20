da74ffd11547cc2401cb51f30999e6d279b0f884
imlaf <lafnouneimane@gmail.com>
2016-10-20 10:56:48 +0200

    Update depart.pas

--- depart.pas
+++ depart.pas
@@ -1,25 +1,15 @@
 unit depart;
 
 interface
-const MAX = 8;
-type Joueur = Record
-		pseudo : String;
-		budget : Integer;
-		ordre : Integer;
-		pion : Char;
-		coordx : Integer;
-		coordy : Integer;
-		possession : array [1..2,1..30] of Integer;
-		
-	end;
-type Team = array  [1..MAX]  of Joueur;
+
+uses init,crt;
 
 implementation
 
-procedure CaseDepart (ndj: Integer; t: Team);
+procedure CaseDepart (ndj: Integer; T: Team);
 
 begin
-with t[ndj] do
+with T[ndj] do
 	budget:= budget + 200 ;
 end;
 
