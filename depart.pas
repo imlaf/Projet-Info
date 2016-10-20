a4be54737c66bfbe54a9b7133a80ab1752b4feae
imlaf <lafnouneimane@gmail.com>
2016-10-20 10:50:57 +0200

    Add files via upload

---
+++ depart.pas
@@ -0,0 +1,26 @@
+unit Jeu1;
+
+interface
+const MAX = 8;
+type Joueur = Record
+		pseudo : String;
+		budget : Integer;
+		ordre : Integer;
+		pion : Char;
+		coordx : Integer;
+		coordy : Integer;
+		possession : array [1..2,1..30] of Integer;
+		
+	end;
+type Team = array  [1..MAX]  of Joueur;
+
+implementation
+
+procedure CaseDepart (ndj: Integer; t: Team);
+
+begin
+with t[ndj] do
+	budget:= budget + 200 ;
+end;
+
+end.
